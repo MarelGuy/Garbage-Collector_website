58c78c6b5aa49a03bbcec978f3c662bbbed52c01
Marelguy <golans.gaming@gmail.com>
2022-04-02 23:42:37 +0200

    kinda done

--- src/App.jsx
+++ src/App.jsx
@@ -10,72 +10,115 @@ const scrollToComponent = (ref) => {
 function App() {
   return (
     <Container>
-      <Image fluid={true} src="../Assets/logomenu.png" className="bg_img" />
+      <section>
+        <Image fluid={true} src="../Assets/logomenu.png" className="bg_img center_div" />
 
-      <p>
-        Il mondo è in rovina, per anni, decenni la terra è stata maltrattata venendo inquinata
-        e diventando una grande discarica a cielo aperto.
-        <br />
-        L’unico eroe che può rimediare
-        al problema è il sintetico
-        pulitore Garbage Collector,
-        Con la sua bagshooter4000.
-      </p>
+        <p>
+          Il mondo è in rovina, per anni, decenni la terra è stata maltrattata venendo inquinata
+          e diventando una grande discarica a cielo aperto.
+          <br />
+          L’unico eroe che può rimediare
+          al problema è il sintetico
+          pulitore Garbage Collector,
+          con la sua bagshooter4000.
+        </p>
 
-      <Button id="scroll_button" onClick={() => {
-        scrollToComponent("scrollTo");
-      }}>Scopri di più</Button>
+        <Button id="scroll_button" className="center_div" onClick={() => {
+          scrollToComponent("scrollTo");
+        }}>Scopri di più</Button>
+      </section>
+      <section>
+        <p style={{ "marginTop": "61vh" }} id="scrollTo"><br /></p>
+        <h2>Le mappe</h2>
 
-      <p style={{ "marginTop": "61vh" }} id="scrollTo"><br /></p>
-      <h2>Le mappe</h2>
+        <Row className="center_div">
+          <Col className='map'><Image fluid={true} src="../Assets/maps/city.png" /></Col>
+          <Col className='map'><Image fluid={true} src="../Assets/maps/stage2.png" /></Col>
+        </Row>
+      </section>
+      <section>
+        <h2 style={{ "marginTop": "10vh" }}>Il personaggio</h2>
 
-      <Row className="center_div">
-        <Col className='map'><Image fluid={true} src="https://maranatha.camp/wp-content/uploads/2016/03/placeholder-1280-600.png" /></Col>
-        <Col className='map'><Image fluid={true} src="https://maranatha.camp/wp-content/uploads/2016/03/placeholder-1280-600.png" /></Col>
-      </Row>
-
-      <h2 style={{ "marginTop": "100vh" }}>Gli oggetti</h2>
-
-      <div className="center_div">
         <Row>
-          <Col>
-            <Card className="card_body center_div armor">
-              <Image className="center_div" fluid={true} src="../Assets/powerup/armor.png" width="200vw" />
-              <Card.Body>
-                <Card.Title>Armatura</Card.Title>
-                <hr />
-                <Card.Text className="armor">
-                  L'Armatura permette al Garbage Collector di resistere ad un danno dei minions.
-                </Card.Text>
-              </Card.Body>
-            </Card>
+          <Col className="center_div">
+            <p>il Garbage-Collector è un Sintetico Addetto alla pulizia creato dalla razza umana come ultima speranza
+              contro la sempre più grande minaccia dell'inquinamento, grazie alla sua spara-buste biodegradabili.</p>
           </Col>
           <Col>
-            <Card className="card_body center_div">
-              <Image className="center_div" fluid={true} src="../Assets/powerup/booster.png" width="200vw" />
-              <Card.Body>
-                <Card.Title>Booster</Card.Title>
-                <hr />
-                <Card.Text>
-                  Hai finito le munizioni? Nessun problema! Grazie al booster il tuo caricatore diventerà come nuovo.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card className="card_body center_div scarpe">
-              <Image className="center_div" fluid={true} src="../Assets/powerup/scarpe.png" width="200vw" />
-              <Card.Body>
-                <Card.Title>Scarpe</Card.Title>
-                <hr />
-                <Card.Text className="scarpe">
-                  Le Scarpe, ti permettono per un breve tempo di essere più agile
-                </Card.Text>
-              </Card.Body>
-            </Card>
+            <Image className="center_div" fluid={true} src="../Assets/garbage collector.png" />
           </Col>
         </Row>
-      </div>
+      </section>
+      <section>
+        <h2 style={{ "marginTop": "10vh" }}>Gli oggetti</h2>
+
+        <div className="center_div">
+          <Row>
+            <Col>
+              <Card className="card_body center_div armor">
+                <Image className="center_div" fluid={true} src="../Assets/powerup/armor.png" width="200vw" />
+                <Card.Body>
+                  <Card.Title>Armatura</Card.Title>
+                  <hr />
+                  <Card.Text >
+                    L'Armatura permette al Garbage Collector di resistere ad un danno dei minions.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="card_body center_div">
+                <Image className="center_div" fluid={true} src="../Assets/powerup/booster.png" width="200vw" />
+                <Card.Body>
+                  <Card.Title>Booster</Card.Title>
+                  <hr />
+                  <Card.Text>
+                    Hai finito le munizioni? Nessun problema! Grazie al booster il tuo caricatore diventerà come nuovo.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card className="card_body center_div scarpe">
+                <Image className="center_div" fluid={true} src="../Assets/powerup/scarpe.png" width="200vw" />
+                <Card.Body>
+                  <Card.Title>Scarpe</Card.Title>
+                  <hr />
+                  <Card.Text>
+                    Le Scarpe, ti permettono per un breve tempo di essere più agile
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </section>
+      <section>
+        <h2 style={{ "marginTop": "10vh" }}>Work in progress</h2>
+
+        <Row>
+          <Col>
+            <Image fluid={true} className="center_div" src="../Assets/WIP/Things/WIP_thing1.png" />
+          </Col>
+          <Col>
+            <Image fluid={true} className="center_div" src="../Assets/WIP/Things/WIP_thing2.png" />
+          </Col>
+        </Row>
+        <Row style={{ "paddingTop": "2.5vh" }}>
+          <Col>
+            <Image fluid={true} className="center_div" src="../Assets/WIP/Maps/WIP_map1.jpg" />
+          </Col>
+          <Col>
+            <Image fluid={true} className="center_div" src="../Assets/WIP/Maps/WIP_map2.jpg" />
+          </Col>
+        </Row>
+      </section>
+      <section style={{ "marginTop": "3vh" }}>
+        {/* footer with credits*/}
+        <footer>
+          <p className="text-center">Made by Loris Cuntreri © {new Date().getFullYear()}</p>
+        </footer>
+      </section>
     </Container>
   )
 }
